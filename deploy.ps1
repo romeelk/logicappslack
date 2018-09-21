7f9e60b3208dc53b8ce3c1185a6e9a445805a79b
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-09-21 18:42:35 +0100

    Added VM need to dynamically link alert to webhook

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
-$resourceGroup = "ms-workshop-logica-app"
+$resourceGroup = "ms-workshop-logic-app"
 New-AzureRmResourceGroup -Name $resourceGroup -Location "West Europe"
 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile ./template.json -TemplateParameterFile ./parameters.json
