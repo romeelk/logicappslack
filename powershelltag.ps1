982aea5173ac57faf61f6ead0432f4c630101790
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-09-26 13:53:37 +0100

    added posh script for tag demo

---
+++ powershelltag.ps1
@@ -0,0 +1,41 @@
+
+$resourceGroupName = "ms-workshop-posh-demo"
+$location = "West Europe"
+$vnetName = 'msworkshop-vnet'
+$tag = @{"msworkshop"="demo"}
+$publicIpName = "rackspacemsworkshop"
+$dnsPrefix = "rackspacemsworkshopdemo"
+$storageaccountName = "rackspacedemostorage"
+$storageAccountNum = (1,2,3,4)
+# Create resource group and a storage account
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+Write-Host "Creating Resource Group -->" $resourceGroupName -ForegroundColor Green
+
+
+foreach ($item in $storageAccountNum) {
+    $name = $storageaccountName+$item
+    New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $name -SkuName "Standard_LRS" -Location $location -ErrorAction "Stop"
+    Write-Host "Creating storage account -->" $name -ForegroundColor Green
+}
+
+# Create a subnet 
+$subnet = New-AzureRmVirtualNetworkSubnetConfig -Name 'frontendSubnet' `
+    -AddressPrefix "10.0.1.0/24" 
+
+# Create a vnet
+$virtualNetwork = New-AzureRmVirtualNetwork `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -Name $vnetName `
+  -AddressPrefix 10.0.0.0/16 `
+  -Tag $tag `
+  -Subnet $subnet
+
+  Write-Host "Creating vnet -->" $vnetName -ForegroundColor Green
+$virtualNetwork | Set-AzureRmVirtualNetwork
+
+$publicIp = New-AzureRmPublicIpAddress -AllocationMethod Static -Name $publicIpName -ResourceGroupName $resourceGroupName -DomainNameLabel $dnsPrefix -Location $location -Tag $tag
+
+Write-Host "Creating public ip -->" $resourceGroupName -ForegroundColor Green
+
+Write-Host "Public IP is: " $publicIp.IpAddress.ToString()
