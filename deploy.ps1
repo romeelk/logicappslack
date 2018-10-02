f996454a9047a37058d691aeb99e7a61b370081b
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-10-02 14:13:24 +0100

    changed location to northeurope

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,6 @@ $pscredential = Get-Credential -UserName $servicePrincipalAppId
 Connect-AzureRmAccount  -Credential $pscredential -ServicePrincipal -TenantId "570057f4-73ef-41c8-bcbb-08db2fc15c2b"  -ErrorAction Stop
 
 $resourceGroup = "ms-workshop-logic-app"
-New-AzureRmResourceGroup -Name $resourceGroup -Location "West Europe"
+New-AzureRmResourceGroup -Name $resourceGroup -Location "North Europe"
 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile ./template.json -TemplateParameterFile ./parameters.json
