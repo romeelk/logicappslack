0623b017ec4d0459e50a9b3116a147e71b67817b
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-10-02 13:04:58 +0100

    added service principal to other demos

--- securitycenter.ps1
+++ securitycenter.ps1
@@ -1,7 +1,8 @@
-
 $authUrl = "https://login.microsoftonline.com/570057f4-73ef-41c8-bcbb-08db2fc15c2b/oauth2/token"
-$postParams = @{grant_type='client_credentials';client_id='f74fb3f0-0c9f-410a-b0e0-d4e556265672';
-client_secret='pHvbMYQDXib7Evj/7h0qZeSG6ROwqWzDKTsJVM02GM4=';resource='https://management.core.windows.net'}
+$clientId = "f74fb3f0-0c9f-410a-b0e0-d4e556265672"
+$credentials = Get-Credential -UserName $clientId
+$postParams = @{grant_type='client_credentials';client_id=$clientId;
+client_secret=$credentials.GetNetworkCredential().Password;resource='https://management.core.windows.net'}
 
 $response = Invoke-WebRequest `
 -Uri  $authUrl `
