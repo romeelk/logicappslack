7f8e9836dcac8328c25a9a057cb270bfcbef97d2
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-10-02 10:27:32 +0100

    added service principal for logon

--- powershelltag.ps1
+++ powershelltag.ps1
@@ -1,4 +1,7 @@
-
+$servicePrincipalAppId = "f74fb3f0-0c9f-410a-b0e0-d4e556265672"
+$key = "J61mu5SLcSf1FikeCjTBYOSRpsBfO5IANJfc8eTIf74="
+$pscredential = Get-Credential -UserName $servicePrincipalAppId
+Connect-AzureRmAccount  -Credential $pscredential -ServicePrincipal -TenantId "570057f4-73ef-41c8-bcbb-08db2fc15c2b"  -ErrorAction Stop
 $resourceGroupName = "ms-workshop-posh-demo"
 $location = "West Europe"
 $vnetName = 'msworkshop-vnet'
