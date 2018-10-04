3cf796a0179e3409e3b29e62ab7700850f5e03e8
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-10-04 21:25:22 +0100

    Chaged region to North Europe

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,10 @@
+$servicePrincipalAppId = "f74fb3f0-0c9f-410a-b0e0-d4e556265672"
 $pscredential = Get-Credential -UserName $servicePrincipalAppId
+$resourceGroup = "ms-workshop-logic-app"
+$location  = "North Europe"
+
 Connect-AzureRmAccount  -Credential $pscredential -ServicePrincipal -TenantId "570057f4-73ef-41c8-bcbb-08db2fc15c2b"  -ErrorAction Stop
 
-$resourceGroup = "ms-workshop-logic-app"
-New-AzureRmResourceGroup -Name $resourceGroup -Location "North Europe"
+New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile ./template.json -TemplateParameterFile ./parameters.json
--- powershelltag.ps1
+++ powershelltag.ps1
@@ -2,12 +2,12 @@ $servicePrincipalAppId = "f74fb3f0-0c9f-410a-b0e0-d4e556265672"
 $pscredential = Get-Credential -UserName $servicePrincipalAppId
 Connect-AzureRmAccount  -Credential $pscredential -ServicePrincipal -TenantId "570057f4-73ef-41c8-bcbb-08db2fc15c2b"  -ErrorAction Stop
 $resourceGroupName = "ms-workshop-posh-demo"
-$location = "West Europe"
-$vnetName = 'msworkshop-vnet'
+$location = "North Europe"
+$vnetName = 'msworkshop-vnet-demo'
 $tag = @{"msworkshop"="demo"}
-$publicIpName = "rackspacemsworkshop"
-$dnsPrefix = "rackspacemsworkshopdemo"
-$storageaccountName = "rackspacedemostorage"
+$publicIpName = "rackspacemsworkshopdemo"
+$dnsPrefix = "rackspacemsworkshopdemo1"
+$storageaccountName = "rackspacedemostoragems"
 $storageAccountNum = (1,2,3,4)
 
 # Create resource group and a storage account
